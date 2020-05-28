0feaa3dc2fd6c9447eeaec1411bf1ee19d5d1408
broglhub <broglhubdonate@gmail.com>
2020-05-28 11:35:26 +0200

    sadasgrs

---
+++ wl.lua
@@ -0,0 +1,10 @@
+local p = game.Players.LocalPlayer
+local whitelist = {["Shadows_Overlord"] = true, ["fhypoo_2"] = true, ["Bananas_Apples2"] = true, ["Trxshba_g"] = true, ["ChemicalKit"] = true}
+
+if whitelist [p.Name] then
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "BroglHub",Text = "You are whitelisted, Enjoy! Made by FlareX, Void, and Fhy",Duration = 5})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "BroglHub",Text = "Make sure you don't leak the script yourself or else you're going to get blacklisted!",Duration = 5})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Also don't forget to join!",Text = "https://discord.gg/xQhEwz8",Duration = 5})
+else
+game.Players.LocalPlayer:Kick("You're not whitelisted! dm a co owner to get yourself whitelisted :)")
+end
