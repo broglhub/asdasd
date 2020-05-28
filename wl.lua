6831aa87cbc23ec45de1fe739081293bbe5123d2
broglhub <broglhubdonate@gmail.com>
2020-05-28 12:23:45 +0200

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -1,5 +1,5 @@
 local p = game.Players.LocalPlayer
-local whitelist = {["Shadows_Overlord"] = true, ["fhypoo_2"] = true, ["Bananas_Apples2"] = true, ["Trxshba_g"] = true, ["ChemicalKit"] = true, ["ArticGamerNL"] = true}
+local whitelist = {["Shadows_Overlord"] = true, ["fhypoo_2"] = true, ["Bananas_Apples2"] = true, ["Trxshba_g"] = true, ["ChemicalKit"] = true, ["ArticGamerNL"] = true, ["vxslw"] = true}
 
 if whitelist [p.Name] then
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "BroglHub",Text = "You are whitelisted, Enjoy! Made by FlareX, Void, and Fhy",Duration = 5})
